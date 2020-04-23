bfbc22a942a2eaacf530a390cf37abf1dfb2327c
Neel Mazumdar <neelmazumdar7@gmail.com>
2020-04-23 00:30:32 -0400

    completion

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,18 +5,47 @@ module.exports = {
   get,
 };
 
+let item = {
+  durability: 0,
+  enhancement: 0
+}
+
 function succeed(item) {
+  if (item.enhancement < 20) {
+    item.enhancement = item.enhancement + 1;
+  } else {
+    item.enhancement = 20;
+  }
   return { ...item };
 }
 
 function fail(item) {
+  if (item.enhancement < 15 ) {
+    item.durability = item.durability - 5;
+  }
+  else {
+    item.durability = item.durability - 10;
+  }
+
+  if (item.enhancement > 16) {
+    item.enhancement = item.enhancement - 1;
+  }
+  else {
+    item.enhancement = item.enhancement;
+  }
+  
   return { ...item };
 }
 
 function repair(item) {
+  
+  item.durability = 100;
   return { ...item };
 }
 
 function get(item) {
+  if (item.enhancement > 0) {
+    item.name = `[+${item.enhancement}]${item.name}`
+  }
   return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,62 @@
-const enhancer = require('./enhancer.js');
+const { repair, succeed, fail, get} = require('./enhancer.js');
 // test away!
+describe("enhancer.js", () => {
+    it("should run test without errors", ()=>{
+        expect(true).toBe(true);
+    })
+
+    
+
+    describe(".repair()", function()  {
+        it("should repair an item to 100 durability", function () {
+            let armor = {};
+            armor.durability = 45;
+            const repaired = repair(armor);
+            expect(repaired.durability).toBe(100);
+        })
+    })
+
+    describe(".succeed()", function()  {
+        it("enhances an item by 1", function () {
+            let vase = {};
+            vase.enhancement = 20;
+            const enhanced = succeed(vase);
+            expect(enhanced.enhancement).toBe(20);
+        })
+    })
+
+    describe(".fail()", function() {
+        it("item loses durability", function () {
+            let helmet = {};
+            
+            helmet.enhancement = 16;
+            helmet.durability = 14;
+
+            const failed = fail(helmet);
+
+           
+            expect(failed.durability).toBe(4);
+        })
+
+        it("item loses enhancement", function () {
+            let helmet = {};
+            
+            helmet.enhancement = 18;
+
+            const failed = fail(helmet);
+
+           
+            expect(failed.enhancement).toBe(17);
+        })
+    })
+
+    describe(".get()", function()  {
+        it("enhances + sword", function () {
+            let sword = {};
+            sword.enhancement = 5;
+            sword.name = "excalibur";
+            const newName = get(sword);
+            expect(newName.name).toBe('[+5]excalibur');
+        })
+    })
+})
